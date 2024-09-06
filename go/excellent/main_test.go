a684153587bd7f0edb54832474a533809fef83e7
habanero2012 <sato1101@live.jp>
2024-09-06 19:51:00 +0900

    sum

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -8,3 +8,10 @@ func TestEvenOrOdd(t *testing.T) {
 		t.Errorf("expected: even, actual: %s", result)
 	}
 }
+
+func TestSum(t *testing.T) {
+	result := sum(10, 20)
+	if result != 30 {
+		t.Errorf("expected: even, actual: %d", result)
+	}
+}
